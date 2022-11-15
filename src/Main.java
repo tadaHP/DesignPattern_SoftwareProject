1a6c89b483fe91901a73a88e01abc186a622beb0
tadaHP <gusvy0220@gmail.com>
2022-11-15 23:07:52 +0900

    Finish checking run on laptop

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,6 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
         UserFactory hanbat = new HanbatUserFactory();
         UserFactory chungnam = new ChungnamUserFactory();
         UserFactory hannam = new HannamUserFactory();
