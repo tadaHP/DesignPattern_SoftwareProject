bbb66ccea7127a881dc3e249d2b1eedbbe01bd88
tadaHP <gusvy0220@gmail.com>
2022-11-15 18:04:20 +0900

    AfterMaking Signup with FactoryMethodPattern

---
+++ src/Builder_GameList/Game.java
@@ -0,0 +1,87 @@
+package Builder_GameList;
+
+import java.util.Date;
+
+public class Game {
+
+    //필수 파라미터
+    private String gameName;
+    private Date gameDate;
+    private String place;
+
+    //선택 파라미터
+    private String description;
+    private String clubName;
+    private String courtType;
+
+    private Game(GameBuilder builder) {
+        this.gameName = builder.gameName;
+        this.gameDate = builder.gameDate;
+        this.place = builder.place;
+        this.description = builder.description;
+        this.clubName = builder.clubName;
+        this.courtType = builder.clubName;
+    }
+
+    public static GameBuilder builder() {
+        return new GameBuilder();
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "gameName: " + gameName + " gameDate: " + gameDate + " place: " + place + " description: " + description + " clubName: " + clubName + " courtType: " + courtType;
+    }
+
+    public static class GameBuilder {
+
+        private String gameName;
+        private Date gameDate;
+        private String place;
+
+        private String description;
+        private String clubName;
+        private String courtType;
+
+        private GameBuilder(){}
+
+        public GameBuilder gameName(String gameName) {
+            this.gameName = gameName;
+            return this;
+        }
+
+        public GameBuilder gameDate(Date gameDate) {
+            this.gameDate = gameDate;
+            return this;
+        }
+
+        public GameBuilder place(String place) {
+            this.place = place;
+            return this;
+        }
+
+        public GameBuilder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public GameBuilder clubName(String clubName) {
+            this.clubName = clubName;
+            return this;
+        }
+
+        public GameBuilder courtType(String courtType) {
+            this.courtType = courtType;
+            return this;
+        }
+
+        public Game build() {
+            return new Game(this);
+        }
+
+    }
+
+
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import Builder_GameList.Game;
 import FactoryMethod_Member.*;
 import FactoryMethod_Member.UserFactory.*;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,7 +12,7 @@ public class Main {
         UserFactory hanbat = new HanbatUserFactory();
         UserFactory chungnam = new ChungnamUserFactory();
         UserFactory hannam = new HanbatUserFactory();
-
+///////////////////////////// TODO: 회원가입 커스텀 필요
 //        User newHanbat = hanbat.newInstance();
 //        User newChungnam = chungnam.newInstance();
 //        User newHannam = hannam.newInstance();
@@ -16,6 +20,32 @@ public class Main {
 //        newHanbat.signup();
 //        newChungnam.signup();
 //        newHannam.signup();
+////////////////////////////////////
+
+
+        //////////////////Builder TODO: 리스트로 만들어서 관리 필요
+
+        Game game1 = Game.builder()
+                .gameName("Test")
+                .gameDate(new Date())
+                .place("코트장")
+                .description("TestDesc")
+                .clubName("Null")
+                .courtType("Grass")
+                .build();
+
+        Game game2 = Game.builder()
+                .gameName("Test")
+                .gameDate(new Date())
+                .place("코트장")
+                        .build();
+
+        System.out.println(game1.toString());
+        System.out.println(game2.toString());
+
+
+        //////////////////
+
 
     }
 }
